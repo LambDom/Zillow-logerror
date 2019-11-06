f34e7d7cfef955eae6e09171ace253479e5a4c67
Fredrick Lambuth <flambuth@gmail.com>
2019-11-06 12:05:10 -0600

    adding outlier functions to prepare.py

--- prepare.py
+++ prepare.py
@@ -32,3 +32,28 @@ def handle_missing_values(df, prop_required_column= .95, prop_required_row = .8)
     df = df.dropna( thresh = thresh )
     
     return df
+
+
+def find_upper_outliers(column):
+    '''
+    Give it a Pandas Series/Column. This will return a a Series of values that 
+    are 1.5X above the .75 quantile
+    '''
+    #Using the quantile function of a Series, lower and upper side of the IQR box is defined.
+    q1, q3 = column.quantile([.25, .75])
+    iqr = q3 - q1
+    #IQR is all the values in the box made bewteen .25-.75 of the data. The middle 50.
+    
+    return column[column < (q3 + 1.5*iqr)]
+
+def find_lower_outliers(column):
+    '''
+    Give it a Pandas Series/Column. This will return a Series of values that 
+    are 1.5X above the .75 quantile
+    '''
+    #Using the quantile function of a Series, lower and upper side of the IQR box is defined.
+    q1, q3 = column.quantile([.25, .75])
+    iqr = q3 - q1
+    #IQR is all the values in the box made bewteen .25-.75 of the data. The middle 50.
+    
+    return column[column < (q1 - 1.5*iqr)]
