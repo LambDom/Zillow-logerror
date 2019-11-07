dfb91f624fdf9d54900efe0dbaef850e33bc9b26
Fredrick Lambuth <flambuth@gmail.com>
2019-11-07 14:37:36 -0600

    cluster module has a function that shows the clusters on the map

--- cluster.py
+++ cluster.py
@@ -14,16 +14,61 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
-#ACQUIRE
-df = pd.read_csv('data.csv')
+def show_clusters_on_map():
 
-#The Master DataFrame of the Latitude and Longitude
-lat_long = df[['latitude','longitude']]
 
-#The columns that have to do with the building themselves. No location data except for lat-long.
-#12 columns
-house_vars = ['fullbathcnt','bathroomcnt','bedroomcnt','calculatedfinishedsquarefeet',
-              'heatingorsystemtypeid','lotsizesquarefeet','yearbuilt','structuretaxvaluedollarcnt', 
-              'taxvaluedollarcnt','latitude','longitude','logerror']
+    #ACQUIRE
+    #########
 
-structures_df = df[house_vars]
+    df = pd.read_csv('data.csv')
+
+    #The Master DataFrame of the Latitude and Longitude
+    lat_long = df[['latitude','longitude']]
+
+
+    #CLEAN
+    ######
+
+    #The columns that have to do with the building themselves. No location data except for lat-long.
+    #12 columns
+    house_vars = ['fullbathcnt','bathroomcnt','bedroomcnt','calculatedfinishedsquarefeet',
+                'heatingorsystemtypeid','lotsizesquarefeet','yearbuilt','structuretaxvaluedollarcnt', 
+                'taxvaluedollarcnt','latitude','longitude','logerror']
+
+    #Impute the mode in heatingorsystemtypeid. Dropped null records after that
+    structures_df = df[house_vars]
+    structures_df.heatingorsystemtypeid.fillna(2, inplace=True)
+    structures_df.dropna(inplace=True)
+
+    #PREPARE
+    ########
+
+    train, test = train_test_split(structures_df, train_size = .8, random_state = 123)
+
+    #Get rid of the lat and long before the scaling happens. lat_long holds this information. We set that in ACQUIRE phase
+    train.drop(['latitude','longitude'],axis=1,inplace=True)
+    test.drop(['latitude','longitude'],axis=1,inplace=True)
+
+    standard_train, standard_test, standard_object = prepare.standardize_train_test(train, test)
+
+    #no_outliers == standard_train without upper outiers for big houses or big lots
+    no_outliers = prepare.remove_upper_outliers(standard_train.calculatedfinishedsquarefeet, train)
+    no_outliers = prepare.remove_upper_outliers(standard_train.lotsizesquarefeet, train)
+
+
+    #MODELING
+    #########
+
+    kmean = KMeans(n_clusters=3)
+    kmean.fit(no_outliers)
+
+    predictions3 = kmean.labels_
+    no_outliers['cluster_labels3'] = predictions3
+
+    #THESE SHOW SOME RESULTS ABOUT THE CLUSTERS. Need to package them into functions.
+    # np.unique(predictions3, return_counts=True)
+    # standard_train.groupby('cluster_labels3').mean()
+
+    no_outliers[['latitude','longitude']] = lat_long
+
+    sns.scatterplot(data=no_outliers, x='longitude', y='latitude', hue='cluster_labels3')
